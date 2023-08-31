feb95051dcda90dcd3720292d6efd9c0042f90ac
haywang <30918019+haywang@users.noreply.github.com>
2023-08-31 10:46:41 +0800

    test

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
     <button
       class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
     >
-      ...
+      test btn
     </button>
     <div class="container mx-auto">Main content</div>
   </div>
